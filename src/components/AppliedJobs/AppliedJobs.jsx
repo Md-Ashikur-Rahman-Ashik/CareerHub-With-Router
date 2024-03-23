093d136d7478834e7ee07df0e91598309ff7e1b2
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-03-23 13:05:24 +0600

    Module finished

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -6,6 +6,23 @@ const AppliedJobs = () => {
   const jobs = useLoaderData();
 
   const [appliedJobs, setAppliedJobs] = useState([]);
+  const [displayJobs, setDisplayJobs] = useState([]);
+
+  const handleJobsFilter = (jobsFilter) => {
+    if (jobsFilter === "all") {
+      setDisplayJobs(appliedJobs);
+    } else if (jobsFilter === "remote") {
+      const remoteJobs = appliedJobs.filter(
+        (jobs) => jobs.remote_or_onsite === "Remote"
+      );
+      setDisplayJobs(remoteJobs);
+    } else if (jobsFilter === "onsite") {
+      const onsiteJobs = appliedJobs.filter(
+        (jobs) => jobs.remote_or_onsite === "Onsite"
+      );
+      setDisplayJobs(onsiteJobs);
+    }
+  };
 
   useEffect(() => {
     const storedJobIds = getStoredJobApplication();
@@ -14,8 +31,9 @@ const AppliedJobs = () => {
       const jobsApplied = jobs.filter((job) => storedJobIds.includes(job.id));
       // console.log(jobs, storedJobIds, jobsApplied)
       setAppliedJobs(jobsApplied);
+      setDisplayJobs(jobsApplied);
     }
-  }, []);
+  }, [jobs]);
 
   return (
     <div>
@@ -23,19 +41,19 @@ const AppliedJobs = () => {
       <details className="dropdown">
         <summary className="m-1 btn">open or close</summary>
         <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-          <li>
+          <li onClick={() => handleJobsFilter("all")}>
             <a>All</a>
           </li>
-          <li>
+          <li onClick={() => handleJobsFilter("remote")}>
             <a>Remote</a>
           </li>
-          <li>
+          <li onClick={() => handleJobsFilter("onsite")}>
             <a>OnSite</a>
           </li>
         </ul>
       </details>
       <ul>
-        {appliedJobs.map((job) => (
+        {displayJobs.map((job) => (
           <li key={job.id}>
             <span>
               {job.job_title} {job.company_name}: {job.remote_or_onsite}
